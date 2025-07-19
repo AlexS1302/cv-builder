36984740ea16315ff791e77aafc38ebabd365846
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-07-19 08:59:14 +0100

    Implement initial layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 //import { useState } from 'react'
-import './styles/App.css'
+import './styles/App.css';
+import Header from './components/Header';
+import CVForm from './components/CVForm';
+import Preview from './components/Preview';
 
 function App() {
 
   return (
-    <></>
-  )
+    <div className='App'>
+      <Header />
+      <CVForm />
+      <Preview />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CVForm.jsx
@@ -0,0 +1,9 @@
+import '../styles/CVForm.css'
+
+function CVForm() {
+  return (
+    <div className="CVForm">CVForm</div>
+  )
+}
+
+export default CVForm
